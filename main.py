5bb5df4d9deff8f93e98149bffc9f9f330561ed3
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-04-29 20:14:43 +0200

    a 	modified:   main.py

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
-from unittest import result
-from fastapi import FastAPI, requests
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
-import deepl
-from requests import request
-
-from app import traductor
+from app.traductor import traductor_func
+from app.video import video_func
 
 app = FastAPI()
+
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: str
+
+@app.get("/")
+def index():
+    return {"message" : "Hola, Pythonianos"}
+
+@app.get("/libros/({id}")
+def mostrar_libro(id: int):
+    return {"data": id}
+
+@app.get("/libros")
+def mostrar_libros():
+    return {"data": "Lista de libros"}
+
+@app.post("/libros")
+def insertar_libro(libro: Libro):
+    return {"message": f"libro (libro.titulo) insertado"}
+
+@app.put("/libros/{id}")
+def actualizar_libro(id: int, libro: Libro):
+    return {"message": f"libro (libro.titulo) actualizado"}
+
+@app.delete("/libros/{id}")
+def eliminar_libro(id: int):
+    return {"message": f"libro (libro.titulo) eliminado"}
+
 
 class TraductorRequest(BaseModel):
     translate_text: str
     target_lang: str
 
+@app.get("/video")
+def video():
+    return video_func()
 
-@app.route("/", methods=[ 'GET'])
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.route('/traductor', methods=[ 'POST'])
-async def traductor_func(translate_text, target_lang):
-    auth_key = "a4341cbe-a8bd-4ac2-97e4-6bce2574cf8a:fx"
-    translator = deepl.Translator(auth_key)
-    translate_text = request .get_json()
-    target_lang = traductor.target_lang
-    response = requests.post()
-
-    if response.status_code != 200:
-        translate_text = traductor(translate_text)
-        target_lang = traductor(target_lang)
-
-        return({"data": result.text})
+@app.post("/traductor")
+async def traductor_endpoint(request: Request, traductor_data: TraductorRequest):
+    translate_text = traductor_data.translate_text
+    target_lang = traductor_data.target_lang
+    traduccion = traductor_func(translate_text, target_lang)
+    return {"data": traduccion}
 
