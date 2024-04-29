f9f1aa42777dcc11b477afc37d9b6dc1abba5f60
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-04-29 18:50:42 +0200

    a 	modified:   main.py

--- main.py
+++ main.py
@@ -5,6 +5,37 @@ from app.video import video_func
 
 app = FastAPI()
 
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: str
+
+@app.get("/")
+def index():
+    return {"message" : "Hola, Pythonianos"}
+
+@app.get("/libros/({id}")
+def mostrar_libro(id: int):
+    return {"data": id}
+
+@app.get("/libros")
+def mostrar_libros():
+    return {"data": "Lista de libros"}
+
+@app.post("/libros")
+def insertar_libro(libro: Libro):
+    return {"message": f"libro (libro.titulo) insertado"}
+
+@app.put("/libros/{id}")
+def actualizar_libro(id: int, libro: Libro):
+    return {"message": f"libro (libro.titulo) actualizado"}
+
+@app.delete("/libros/{id}")
+def eliminar_libro(id: int):
+    return {"message": f"libro (libro.titulo) eliminado"}
+
+
 class TraductorRequest(BaseModel):
     translate_text: str
     target_lang: str
